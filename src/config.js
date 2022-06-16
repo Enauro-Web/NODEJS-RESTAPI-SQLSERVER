83d1f216cab919df4c01a48860fb3c64546435ca
Rafael OrtizPostigo <ESORTIZPOSTR@tetrapak.com>
2022-06-16 11:12:49 +0200

    20220616_3

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ config();
 //console.log(process.env.PORT)
 export default {
     port: process.env.PORT || 3000,
-    server: process.env.SERVERNAME,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE   
+    server: process.env.SERVERNAME || '',
+    user: process.env.USER || '',
+    password: process.env.PASSWORD || '',
+    database: process.env.DATABASE || ''  
 }
